bfc0bcfd152132183c93a0eaa1cf9d5f83daeac1
AdrianDKZ <100346054@alumnos.uc3m.es>
2018-04-30 14:24:40 +0200

    Inicio Sesion

--- FunFinder_Alpha/src/app/app.component.ts
+++ FunFinder_Alpha/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = CreateEventPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
