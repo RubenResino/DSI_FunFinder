0ed600235099dde419b6e8f5e24024f440211606
AdrianDKZ <100346054@alumnos.uc3m.es>
2018-05-01 21:59:12 +0200

    Comprar Eventos

--- FunFinder/src/pages/comprar-evento/comprar-evento.ts
+++ FunFinder/src/pages/comprar-evento/comprar-evento.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseDbProvider } from './../../providers/firebase-db/firebase-db';
+import { Evento } from './../../models/evento.model';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 
 /**
  * Generated class for the ComprarEventoPage page.
@@ -14,9 +18,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'comprar-evento.html',
 })
 export class ComprarEventoPage {
+  datosEvento:Evento;
+  buyForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public dbFirebase:FirebaseDbProvider,public formBuilder:FormBuilder) 
+  {
+  	this.datosEvento=this.dbFirebase.getEventoById(navParams.get('id'));
+  	this.buyForm = this.formBuilder.group
+  	({
+      Entradas: [''],
+      Total: ['']
+    })
+  
   }
+
+buyEvent()
+{
+	this.buyForm.controls['Total']=this.buyForm.controls['Entradas'].value;
+}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ComprarEventoPage');
